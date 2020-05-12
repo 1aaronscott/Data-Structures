e18a94ecea39d0c2c1fb7c2246f70da9f3f683fa
1aaronscott <1aaronscott@users.noreply.github.com>
2020-05-11 21:16:39 -0500

    queue implemented with array and LL

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,71 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+from linked_list_day_1 import LinkedList
+
+# Part 1 implement stack using an array
+
+
 class Queue:
+    ''' Implement using arrays. Mimic an array by having
+    a maxSize. '''
+
+    def __init__(self):
+        self.queue = list()
+        self.maxSize = 80
+        self.head = 0
+        self.tail = 0
+
+    # Adding elements
+    def enqueue(self, value):
+        # Checking if the queue is full
+        if self.size() >= self.maxSize:
+            return None
+        self.queue.append(value)
+        self.tail += 1
+        return True
+
+    # Deleting elements
+    def dequeue(self):
+        # Checking if the queue is empty
+        if self.size() <= 0:
+            self.resetQueue()
+            return None
+        data = self.queue[self.head]
+        self.head += 1
+        return data
+
+    # Calculate size
+    def __len__(self):
+        return self.tail - self.head
+
+    def size(self):
+        return self.tail - self.head
+
+    # Reset queue
+    def resetQueue(self):
+        self.tail = 0
+        self.head = 0
+        self.queue = list()
+
+
+# Part 2 implementation using a LL
+class LL_Queue:
+    ''' uses the linked list code from lecture to build a queue '''
+
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_end(value)
 
     def dequeue(self):
-        pass
+        if self.storage.head:
+            self.size -= 1
+            return self.storage.remove_from_head()
